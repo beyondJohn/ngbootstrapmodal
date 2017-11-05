e85c77ff0f365e82f5f4cea5ead63ca324a53403
beyondJohn <jpaul@iamxml.com>
2017-11-05 01:03:06 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import {NgbModule, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { Http, HttpModule } from '@angular/http';
+import { NgmodalComponent } from './ngmodal/ngmodal.component';
+
+const approutes: Routes = [
+  {
+    path: 'app',
+    component: AppComponent,
+    data: { title: 'app' }
+  },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgmodalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule,
+    RouterModule.forRoot(
+      approutes
+    ),
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
